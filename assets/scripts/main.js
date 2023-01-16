f02b9ad84b8b4915d59400eacdff77a72bfc0b51
sulbd <joaquinfleitas71@gmail.com>
2023-01-15 22:50:19 -0300

    contacto y footer

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,6 +11,11 @@ createApp({
             valorBusqueda:"",
             valorSelector:"",
             listaCarrito:[],
+            productoDetalles:null,
+            totalCarrito:0,
+            envio:"",
+            cuotas:"",
+            metodoPago:"",
         }
     },
     created(){
@@ -18,13 +23,16 @@ createApp({
         .then(res=> res.json())
         .then(res=>{
             this.articulos=this.DetectarPagina(res);
-            console.log(this.articulos)
             this.articulosFiltrados=this.articulos;
             if(localStorage.getItem("Carrito")){
                 this.listaCarrito= JSON.parse(localStorage.getItem("Carrito"));
+                this.articulosFiltrados= this.ActualizarEstadoCarrito();
+                console.log(this.articulosFiltrados)
             }
+            this.productoDetalles=this.articulos[0];
+            console.log(this.listaCarrito)
         })
-        .catch(err=> this.errorCarga=true);
+        // .catch(err=> this.errorCarga=true);
     },
     methods:{
         DetectarPagina(datos){
@@ -38,27 +46,66 @@ createApp({
                 return datos;
             }
         },
+        ActualizarEstadoCarrito(){
+            return this.articulos.map(arti=>{
+                let index= this.listaCarrito.findIndex(prod=>prod.producto===arti.producto);
+                if(index>=0){
+                    arti.enCarrito=this.listaCarrito[index].enCarrito;
+                }
+                return arti;
+            })
+        },
         FiltrarBusqueda(){
             this.articulosFiltrados= this.articulos.filter(art=> art.producto.toLowerCase().includes(this.valorBusqueda.toLowerCase()));
 
         },
         AgregarAlCarrito(arti){
-            let index= this.listaCarrito.findIndex(prod.producto===arti.producto);
+            let index= this.listaCarrito.findIndex(prod=>prod.producto===arti.producto);
             if (index<0){
+                console.log("pusheado")
                 arti.enCarrito=1;
                 this.listaCarrito.push(arti);
             }else{
+                console.log("agregado")
                 this.listaCarrito[index].enCarrito++;
             }
             localStorage.setItem("Carrito", JSON.stringify(this.listaCarrito));
+            this.articulosFiltrados=this.ActualizarEstadoCarrito();
+            console.log(JSON.parse(localStorage.getItem("Carrito")))
         },
         RestarAlCarrito(arti){
-            let index= this.listaCarrito.findIndex(prod.producto===arti.producto);
+            let index= this.listaCarrito.findIndex(prod=>prod.producto===arti.producto);
             this.listaCarrito[index].enCarrito--;
+            this.articulosFiltrados=this.ActualizarEstadoCarrito();
             if(this.listaCarrito[index].enCarrito<=0){
                 this.listaCarrito.splice(index,1);
             }
             localStorage.setItem("Carrito", JSON.stringify(this.listaCarrito));
+            this.articulosFiltrados=this.ActualizarEstadoCarrito();
+            console.log(JSON.parse(localStorage.getItem("Carrito")))
+        },
+        ManejarDetalles(prod){
+            this.productoDetalles=prod;
+            console.log(this.productoDetalles)
+        },
+        TotalCarrito(){
+            this.totalCarrito=this.listaCarrito.reduce((acu,elem)=>{
+                return (acu + (elem.enCarrito*elem.precio));
+            },0)
+        },
+        ManejarPago(){
+            switch (this.cuotas) {
+                case "1": return (this.totalCarrito +
+                    (this.envio==="domicilio"?500:0));
+                case "3": return (this.totalCarrito +
+                    (this.envio==="domicilio"?500:0))*1.3;
+                case "6": return (this.totalCarrito +
+                    (this.envio==="domicilio"?500:0))*2;
+                case "12": return (this.totalCarrito +
+                    (this.envio==="domicilio"?500:0))*2.5;
+                default: return (this.totalCarrito +
+                    (this.envio==="domicilio"?500:0));
+            }
         },
         OrdenarCartas(){
             if(this.valorSelector==="a-z"){
